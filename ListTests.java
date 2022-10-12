8217f99a9e2941ab2c500487cc2b61c1afcdb33b
Faith-Okamoto <114197852+Faith-Okamoto@users.noreply.github.com>
2022-10-12 15:33:41 -0700

    fixed filter

--- ListTests.java
+++ ListTests.java
@@ -4,12 +4,27 @@ import java.util.*;
 
 import org.junit.*;
 
+class DogChecker implements StringChecker {
+    @Override
+    public boolean checkString(String s) {
+        return s.startsWith("dog");
+    }
+}
+
 public class ListTests {
+    @Test
+    public void testFilter() {
+        List<String> input = new ArrayList<>(Arrays.asList("dog1", "dog2"));
+        StringChecker checker = new DogChecker();
+        assertEquals(new ArrayList<>(Arrays.asList("dog1", "dog2")),
+                        ListExamples.filter(input, checker));
+    }
+
 	@Test 
 	public void testMerge() {
-    List<String> list1 = new ArrayList<>();
-    List<String> list2 = new ArrayList<>(Arrays.asList("hi"));
-    assertEquals(new ArrayList<>(Arrays.asList("hi")), 
-                    ListExamples.merge(list1, list2));
+        List<String> list1 = new ArrayList<>();
+        List<String> list2 = new ArrayList<>(Arrays.asList("hi"));
+        assertEquals(new ArrayList<>(Arrays.asList("hi")), 
+                        ListExamples.merge(list1, list2));
 	}
 }
